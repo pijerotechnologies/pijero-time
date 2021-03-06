c833beb8e83f65fda1e6fdbd02193f9e17786c58
Rūtenis Raila <rutenisraila@hotmail.com>
2021-03-06 15:42:12 +0200

    env updates

--- src/payloads.js
+++ src/payloads.js
@@ -1,121 +1,121 @@
 module.exports = {
-    confirmation: context => {
+    confirmation: (context) => {
         return {
             channel: context.channel_id,
-            text: 'Helpdesk ticket created!',
+            text: "Helpdesk ticket created!",
             blocks: JSON.stringify([
                 {
-                    type: 'section',
+                    type: "section",
                     text: {
-                        type: 'mrkdwn',
-                        text: '*Helpdesk ticket created!*'
-                    }
+                        type: "mrkdwn",
+                        text: "*Helpdesk ticket created!*",
+                    },
                 },
                 {
-                    type: 'divider'
+                    type: "divider",
                 },
                 {
-                    type: 'section',
+                    type: "section",
                     text: {
-                        type: 'mrkdwn',
-                        text: `*Title*\n${context.title}\n\n*Description*\n${context.description}`
-                    }
+                        type: "mrkdwn",
+                        text: `*Title*\n${context.title}\n\n*Description*\n${context.description}`,
+                    },
                 },
                 {
-                    type: 'context',
+                    type: "context",
                     elements: [
                         {
-                            type: 'mrkdwn',
-                            text: `*Urgency*: ${context.urgency}`
-                        }
-                    ]
-                }
-            ])
-        }
+                            type: "mrkdwn",
+                            text: `*Urgency*: ${context.urgency}`,
+                        },
+                    ],
+                },
+            ]),
+        };
     },
-    modal: context => {
+    modal: (context) => {
         return {
             trigger_id: context.trigger_id,
             view: JSON.stringify({
-                type: 'modal',
+                type: "modal",
                 title: {
-                    type: 'plain_text',
-                    text: 'Submit a helpdesk ticket'
+                    type: "plain_text",
+                    text: "Submit a helpdesk ticket",
                 },
-                callback_id: 'submit-ticket',
+                callback_id: "submit-ticket",
                 submit: {
-                    type: 'plain_text',
-                    text: 'Submit'
+                    type: "plain_text",
+                    text: "Submit",
                 },
                 blocks: [
                     {
-                        block_id: 'title_block',
-                        type: 'input',
+                        block_id: "title_block",
+                        type: "input",
                         label: {
-                            type: 'plain_text',
-                            text: 'Title'
+                            type: "plain_text",
+                            text: "Title",
                         },
                         element: {
-                            action_id: 'title',
-                            type: 'plain_text_input'
+                            action_id: "title",
+                            type: "plain_text_input",
                         },
                         hint: {
-                            type: 'plain_text',
-                            text: '30 second summary of the problem'
-                        }
+                            type: "plain_text",
+                            text: "30 second summary of the problem",
+                        },
                     },
                     {
-                        block_id: 'description_block',
-                        type: 'input',
+                        block_id: "description_block",
+                        type: "input",
                         label: {
-                            type: 'plain_text',
-                            text: 'Description'
+                            type: "plain_text",
+                            text: "Description",
                         },
                         element: {
-                            action_id: 'description',
-                            type: 'plain_text_input',
-                            multiline: true
+                            action_id: "description",
+                            type: "plain_text_input",
+                            multiline: true,
                         },
-                        optional: true
+                        optional: true,
                     },
                     {
-                        block_id: 'urgency_block',
-                        type: 'input',
+                        block_id: "urgency_block",
+                        type: "input",
                         label: {
-                            type: 'plain_text',
-                            text: 'Importance'
+                            type: "plain_text",
+                            text: "Importance",
                         },
                         element: {
-                            action_id: 'urgency',
-                            type: 'static_select',
+                            action_id: "urgency",
+                            type: "static_select",
                             options: [
                                 {
                                     text: {
                                         type: "plain_text",
-                                        text: "High"
+                                        text: "High",
                                     },
-                                    value: "high"
+                                    value: "high",
                                 },
                                 {
                                     text: {
                                         type: "plain_text",
-                                        text: "Medium"
+                                        text: "Medium",
                                     },
-                                    value: "medium"
+                                    value: "medium",
                                 },
                                 {
                                     text: {
                                         type: "plain_text",
-                                        text: "Low"
+                                        text: "Low",
                                     },
-                                    value: "low"
-                                }
-                            ]
+                                    value: "low",
+                                },
+                            ],
                         },
-                        optional: true
-                    }
-                ]
-            })
-        }
-    }
-}
+                        optional: true,
+                    },
+                ],
+            }),
+        };
+    },
+};
